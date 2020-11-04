8b8ef6ccda9bd21d647d6fe0f6a2fee5f4cb9b6e
Burke <rbg18@pitt.edu>
2020-11-04 16:00:31 -0500

    Added working Maxwell-Stefano diffusivity calculation with tentative anisotropic calculation too

--- traj_analysis/traj_standalone.py
+++ traj_analysis/traj_standalone.py
@@ -8,6 +8,108 @@ import seaborn as sns
 from ovito.io import import_file
 
 
+def pbc_distance(pos1, pos2, cell, dims = 3):
+    """ Find distance between a set of atoms and their image in another timeframe
+            - this assumes all particles are wrapped inside the simulation box
+            - corrects for image convention if particle wraps to other side
+        Parameters
+        ---------
+        pos1: (n_atoms, dims) ndarray for first set of positions
+        pos2: (n_atoms, dims) ndarray for second set
+        cell: (3,3) ndarray with simulation cell lengths
+        dims (optional): number of dimensions for the calculation
+    """
+    diff = abs(pos1 - pos2)
+    for i in range(dims):
+        # np.where neatly vectorizes the if-else logic for a 2D matrix
+        diff[:,i] = np.where(2*diff[:,i] > cell[i,i], cell[i,i] - diff[:,i], diff[:,i])
+    return diff
+
+def calc_onsagers(disp_list_1, disp_list_2, delta_t, nsamples):
+    """ Calculate onsager coefficients for a binary system to yield
+        a scalar for the Maxwell-Stefan diffusivity
+        Parameters
+        -----------
+        disp_list_1: list of ndarrays w/ shape (natoms,dims) if directional else (natoms,)
+        disp_list_2: list of ndarrays w/ shape (natoms,dims) if directional else (natoms,)
+        delta_t: float, time in picoseconds per traj frame
+        nsamples: int, number of samples for expectation values
+        -----------
+    """
+    ntotatoms = len(disp_list_1[0]) + len(disp_list_2[0])
+    onsager = np.zeros((2,2))
+    onsager_std = np.zeros((2,2))
+    # off-diagonal
+    onsager[0,1] = np.average(np.multiply([np.sum(disp_list_1[i]) for i in range(nsamples)],
+                                          [np.sum(disp_list_2[i]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    # symmetry of onsager coefficients
+    onsager[1,0] = onsager[0,1]
+    # diagonal entries
+    onsager[0,0] = np.average(np.square([np.sum(disp_list_1[i]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager[1,1] = np.average(np.square([np.sum(disp_list_2[i]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    # similar process for the error bounds on this measurement
+    onsager_std[0,1] = np.std(np.multiply([np.sum(disp_list_1[i]) for i in range(nsamples)],
+                                          [np.sum(disp_list_2[i]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std[1,0] = onsager_std[0,1]
+    onsager_std[0,0] = np.std(np.square([np.sum(disp_list_1[i]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std[1,1] = np.std(np.square([np.sum(disp_list_2[i]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    return (onsager, onsager_std)
+
+def calc_directional_onsagers(disp_list_1, disp_list_2, delta_t, nsamples):
+    """ Calculate onsager coefficients for a binary alloy to yield
+        vectors of diffusivity in each direction for orientation analysis
+        Parameters
+        -----------
+        disp_list_1: list of ndarrays w/ shape (natoms,dims) if directional else (natoms,)
+        disp_list_2: list of ndarrays w/ shape (natoms,dims) if directional else (natoms,)
+        delta_t: float, time in picoseconds per traj frame
+        nsamples: int, number of samples for expectation values
+        -----------
+    """
+    # scaling factor
+    ntotatoms = len(disp_list_1[0][:,0]) + len(disp_list_2[0][:,0])
+    # x-direction average
+    onsager_xx = np.zeros((2,2))
+    onsager_xx[0,0] = np.average(np.square([np.sum(disp_list_1[i][:,0]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_xx[1,1] = np.average(np.square([np.sum(disp_list_2[i][:,0]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_xx[0,1] = np.average(np.multiply([np.sum(disp_list_1[i][:,0]) for i in range(nsamples)], [np.sum(disp_list_2[i][:,0]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_xx[1,0] = onsager_xx[0,1]
+    # x-direction
+    # y-direction
+    onsager_yy = np.zeros((2,2))
+    onsager_yy[0,0] = np.average(np.square([np.sum(disp_list_1[i][:,1]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_yy[1,1] = np.average(np.square([np.sum(disp_list_2[i][:,1]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_yy[0,1] = np.average(np.multiply([np.sum(disp_list_1[i][:,1]) for i in range(nsamples)], [np.sum(disp_list_2[i][:,1]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_yy[1,0] = onsager_yy[0,1]
+    # z-direction
+    onsager_zz = np.zeros((2,2))
+    onsager_zz[0,0] = np.average(np.square([np.sum(disp_list_1[i][:,2]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_zz[1,1] = np.average(np.square([np.sum(disp_list_2[i][:,2]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_zz[0,1] = np.average(np.multiply([np.sum(disp_list_1[i][:,2]) for i in range(nsamples)], [np.sum(disp_list_2[i][:,2]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_zz[1,0] = onsager_zz[0,1]
+    # off-diagonal terms
+    #self.onsager_xy = np.average(np.multiply([np.sum(disp_list_1[i][:,0]) for i in range(nsamples)], [np.sum(vector_lengths[i][:,1]) for i in range(nsamples)]))
+    #self.onsager_xz = np.average(np.multiply([np.sum(disp_list_1[i][:,0]) for i in range(nsamples)], [np.sum(disp_list_[i][:,2]) for i in range(nsamples)]))
+    #self.onsager_yz = np.average(np.multiply([np.sum(disp_list_1[i][:,1]) for i in range(nsamples)], [np.sum(disp_list_2[i][:,2]) for i in range(nsamples)]))
+    # standard deviation of this measurement
+    onsager_std_xx = np.zeros((2,2))
+    onsager_std_yy = np.zeros((2,2))
+    onsager_std_zz = np.zeros((2,2))
+    onsager_std_xx[0,0] = np.std(np.square([np.sum(disp_list_1[i][:,0]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_xx[1,1] = np.std(np.square([np.sum(disp_list_2[i][:,0]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_xx[0,1] = np.std(np.multiply([np.sum(disp_list_1[i][:,0]) for i in range(nsamples)], [np.sum(disp_list_2[i][:,0]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_yy[0,0] = np.std(np.square([np.sum(disp_list_1[i][:,1]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_yy[1,1] = np.std(np.square([np.sum(disp_list_2[i][:,1]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_yy[0,1] = np.std(np.multiply([np.sum(disp_list_1[i][:,1]) for i in range(nsamples)], [np.sum(disp_list_2[i][:,1]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_yy[1,0] = onsager_std_yy[0,1]
+    onsager_std_zz[0,0] = np.std(np.square([np.sum(disp_list_1[i][:,2]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_zz[1,1] = np.std(np.square([np.sum(disp_list_2[i][:,2]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_zz[0,1] = np.std(np.multiply([np.sum(disp_list_1[i][:,2]) for i in range(nsamples)], [np.sum(disp_list_2[i][:,2]) for i in range(nsamples)]))/(delta_t*nsamples)/ntotatoms/6
+    onsager_std_zz[1,0] = onsager_std_zz[0,1]
+    onsager = [onsager_xx, onsager_yy, onsager_zz]
+    onsager_std = [onsager_std_xx, onsager_std_yy, onsager_std_zz]
+    return onsager, onsager_std
+
 # general function for pandas dataframe cross-correlation with lag
 def crosscorr(datax, datay, lag=0, wrap=False):
     """ Lag-N cross correlation.
@@ -35,14 +137,15 @@ class TrajStats():
     """
 
 
-    def __init__(self, filename, atomid, rich_atomid, vacid, r = 3.0, parsplice = False):
+    def __init__(self, filename, atomid, rich_atomid, vacid, parsplice = False):
         """
         Parameters
         ----------
         filename: OVITO readable file such as .lmp or .xyz (ideally this is a preprocessed file by the user)
         atomid: integer label for the dopant atom type in the file
+        rich_atomid: integer label for the rich component in the file
         vacid: integer label for the vacancy 'atom type'
-        r: cut-off radius for find_points_in_spheres (pymatgen)
+        parsplice: boolean flag for different time resolutions
         ----------
         """
         self.filename = filename
@@ -53,16 +156,14 @@ class TrajStats():
         self.pipeline = import_file(self.filename, sort_particles = True)
         self.timesteps = self.pipeline.source.num_frames
         data = self.pipeline.compute(0)
-        # parameters for pymatgen's find_points_in_spheres
-        self.r = r
         # 3x3 lattice dimensions
-        self.cell = np.array(data.cell).copy(order = 'C').astype('double')[:3,:3]
-        self.a = np.array([1, 1, 1]).copy(order = 'C')
+        self.cell = np.array(data.cell)[:3,:3]
         # parse this file into a numpy array and pandas dataframe for further study
         # put types and xyz positions into a dictionary
         self.trajs = {}
         self.vactrajs  = {}
         self.atomtrajs = {}
+        self.rich_atomtrajs = {}
         for frame_index in range(0, self.pipeline.source.num_frames):
             data = self.pipeline.compute(frame_index)
             pos = np.array(data.particles['Position'])
@@ -73,13 +174,14 @@ class TrajStats():
             # naive vacancy tracking, probably isnt reliable and needs to be refined with pymatgen loop next
             self.vactrajs[frame_index] = self.trajs[frame_index][np.ma.where(self.trajs[frame_index][:,0] == self.vacid)]
             self.atomtrajs[frame_index] = self.trajs[frame_index][np.ma.where(self.trajs[frame_index][:,0] == self.atomid)]
-            self.rich_atomtrajs[frame_index] = self.trajs[frame_index][.np.ma.where(self.trajs[frame_index][:,0] == self.rich_atomid)]
+            self.rich_atomtrajs[frame_index] = self.trajs[frame_index][np.ma.where(self.trajs[frame_index][:,0] == self.rich_atomid)]
 
         # atoms of interest
         self.atomsvstime = np.array([self.atomtrajs[frame][:,1:] for frame in self.atomtrajs.keys()], dtype = float)
         self.rich_atomsvstime = np.array([self.rich_atomtrajs[frame][:,1:] for frame in self.rich_atomtrajs.keys()], dtype = float)
         self.natoms = len(self.atomsvstime[0,:,0])
         self.nrichatoms = len(self.rich_atomsvstime[0,:,0])
+        self.ntotatoms = self.natoms + self.nrichatoms
         # vacancies only (nvacs required to smooth nested sequence into the same shapes
         # in case there are 0 lattice vacancies in a frame, or some fluctuating number)
         # This fluctuation happens infrequently and can be fixed by propagating the previous frame
@@ -108,7 +210,7 @@ class TrajStats():
         ids = [atom + 1 for atom in range(0,self.natoms)]
         self.richdf = pd.DataFrame(index = range(0, self.pipeline.source.num_frames))
         for atom_id in ids:
-            self.richdf[atom_id] = self.rich_atomsvstime[:, atom_d - 1, 2]
+            self.richdf[atom_id] = self.rich_atomsvstime[:, atom_id - 1, 2]
 
         cols = list(self.richdf)
         self.richvariances = {}
@@ -124,7 +226,8 @@ class TrajStats():
         for col in cols:
             self.vacvariances[col] = self.vacdf.var()[col]
 
-    def Naiveflux(self):
+    # personally designed flux measurement (very rough) for slab models w/ a centerline
+    def naiveflux(self):
         self.centerline = self.cell[2,2]/2
         self.segregated = []
         segregated = []
@@ -145,7 +248,6 @@ class TrajStats():
             elif initial > self.centerline:
                 if initial - final > self.r/2:
                     self.segregated.append(i)
-
         nseg = len(self.segregated)
         # atomic flux in atoms/ang^2/ps (2 ps per 1000 frames and 2A on the slab)
         self.flux = nseg/(2*self.cell[0,0]*self.cell[1,1])/(2*self.timesteps)
@@ -153,12 +255,77 @@ class TrajStats():
         self.flux = self.flux/(1e-20)/(6.02e23)/(1e-12)
         return self.flux
 
-    def MBflux(self, delta_t, nsamples = 20):
+    def msflux(self, delta_t, nsamples, directional = False):
+        """
+        Calculate Maxwell-Stefan diffusivity coefficients using the
+        Onsager reciprocal relations and a measurement of MSD
+        Parameters
+        ===============
+        delta_t: Timestep (in picoseconds) per lammps dump file
+            *determined by LAMMPS dump frequency and internal timestep*
+
+        nsamples: Number of samples for MSD and vector displacement averaging
+
+        directional:  Boolean flag for diffusion tensor
+        ===============
+        """
         self.delta_t = delta_t
+        self.nsamples = nsamples
+        self.directional = directional
         self.binsize = int(self.timesteps/self.nsamples)
+        self.binsize = int(self.timesteps/self.nsamples)
+        #### these should all have len = nsamples
+        # list for MSD samples
+        self.disp_magnitudes = []
+        self.rich_disp_magnitudes = []
+        self.vac_disp_magnitudes = []
+        # lists for vectors of length 3 (samples for x y z flux)
+        self.vector_lengths = []
+        self.rich_vector_lengths = []
+        self.vac_vector_lengths = []
+        # raw differences (mostly stored for debugging)
+        self.diffs = []
+        self.rich_diffs = []
+        self.vac_diffs = []
+        # sliding window displacement collection
         for frame in range(self.binsize, self.timesteps + self.binsize, self.binsize):
-                frame = np.zeros((1,1), dtype = int)
-        return None
+            # PBC distance for all samples (assumes particles are all wrapped into simulation box)
+            self.diffs.append(pbc_distance(self.atomsvstime[frame - self.binsize, :, :], self.atomsvstime[frame - 1, :, :], self.cell))
+            self.rich_diffs.append(pbc_distance(self.rich_atomsvstime[frame - self.binsize, :, :], self.rich_atomsvstime[frame - 1,:,:], self.cell))
+            self.vac_diffs.append(pbc_distance(self.vacsvstime[frame - self.binsize, :, :], self.vacsvstime[frame - 1,:,:], self.cell))
+            # Euclidean norms (displacement)
+            self.disp_magnitudes.append(np.linalg.norm(self.diffs[-1], axis = 1))
+            self.rich_disp_magnitudes.append(np.linalg.norm(self.rich_diffs[-1], axis = 1))
+            self.vac_disp_magnitudes.append(np.linalg.norm(self.vac_diffs[-1], axis = 1))
+            #  Vector magnitudes (for directional flux)
+            self.vector_lengths.append(self.diffs[-1])
+            self.rich_vector_lengths.append(self.rich_diffs[-1])
+            self.vac_vector_lengths.append(self.vac_diffs[-1])
+        # using displacement magnitude (cartesian distance) to get a scalar value for D
+            ## ONSAGER MATRIX ##
+            # onsager[0,0] = Rich element (Cu in current study)
+            # onsager[1,1] = Dilute element (Ni in current study)
+            # onsager[0,1] = onsager[1,0] = Off-diagonal terms (symmetric)
+        self.onsager, self.onsager_std = calc_onsagers(self.rich_disp_magnitudes, self.disp_magnitudes, self.delta_t, self.nsamples)
+        # Concentrations
+        X_rich = self.nrichatoms/self.ntotatoms
+        X_dilute = self.natoms/self.ntotatoms
+        # Maxwell-Stefan diffusivity for binary mixture
+        self.diff = (X_dilute/X_rich)*self.onsager[0,0] + (X_rich/X_dilute)*self.onsager[1,1] - 2*self.onsager[0,1]
+        self.diff_upper = (X_dilute/X_rich)*(self.onsager[0,0]+self.onsager_std[0,0]) + (X_rich/X_dilute)*(self.onsager[1,1]+self.onsager_std[1,1]) - 2*(self.onsager[0,1]+self.onsager_std[0,1])
+        self.diff_lower = (X_dilute/X_rich)*(self.onsager[0,0]-self.onsager_std[0,0]) + (X_rich/X_dilute)*(self.onsager[1,1]-self.onsager_std[1,1]) - 2*(self.onsager[0,1]-self.onsager_std[0,1])
+        print('Diffusivity is ' + str(self.diff) + '\n')
+        print('Upper Confidence bound: ' + str(self.diff_upper) + '\n')
+        print('Lower Confidence bound: ' + str(self.diff_lower) + '\n')
+        self.diffusivity = {'value': self.diff, 'upper': self.diff_upper, 'lower': self.diff_lower}
+        # using the vector magnitudes in each direction to calculate a tensor if requested
+        if directional == True:
+            self.onsager_direct, self.onsager_direct_std = calc_directional_onsagers(self.rich_vector_lengths, self.vector_lengths, self.delta_t, self.nsamples)
+            self.diff_xx = (X_dilute/X_rich)*self.onsager_direct[0][0,0] + (X_rich/X_dilute)*self.onsager_direct[0][1,1] - 2*self.onsager_direct[0][0,1]
+            self.diff_yy = (X_dilute/X_rich)*self.onsager_direct[1][0,0] + (X_rich/X_dilute)*self.onsager_direct[1][1,1] - 2*self.onsager_direct[1][0,1]
+            self.diff_zz = (X_dilute/X_rich)*self.onsager_direct[2][0,0] + (X_rich/X_dilute)*self.onsager_direct[2][1,1] - 2*self.onsager_direct[2][0,1]
+            self.diff_directional = np.sqrt(self.diff_xx**2 + self.diff_yy**2 + self.diff_zz**2)
+        return self.diffusivity
     # keep variances above 0.1 threshold
     def keeping(self, threshold):
         self.keeps = {}
